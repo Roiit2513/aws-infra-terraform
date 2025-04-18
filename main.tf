ea2ed51337d945af0eb8306668070c6779d4e1c8
Rohit <patilrohit2513@gmail.com>
2025-04-18 21:42:09 +0530

    public ip

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "web_sg" {
