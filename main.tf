d0609a57ad9fd7104ae18002130e1bfca59e387e
Rohit <patilrohit2513@gmail.com>
2025-04-18 22:23:22 +0530

    show json on public ip

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
-  
+
   tags = {
     Name = "Btech-FYP"
   }
@@ -51,11 +51,40 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo '{"Name": "Rohit Patil", "Roll Number": "U21EC117", "Project Title": "Automated Deployment of Scalable AWS Infrastructure Using Terraform and Jenkins"}' > /var/www/html/index.html
+              echo 'AddType application/json .html' >> /etc/httpd/conf/httpd.conf
+              systemctl restart httpd
+              EOF
 
   tags = {
     Name = "Terraform-EC2"
