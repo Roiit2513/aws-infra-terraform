f633bae30b3eafd0493da4ef49f0523216e0b5a1
Rohit <patilrohit2513@gmail.com>
2025-04-18 19:25:35 +0530

    apply

--- main.tf
+++ main.tf
@@ -2,60 +2,60 @@ provider "aws" {
   region = var.aws_region
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
-#   tags = {
-#     Name = "Btech-FYP"
-#   }
-# }
+  tags = {
+    Name = "Btech-FYP"
+  }
+}
 
-# resource "aws_subnet" "main" {
-#   vpc_id                  = aws_vpc.main.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "${var.aws_region}a"
-# }
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+}
 
-# resource "aws_security_group" "web_sg" {
-#   name        = "web-sg"
-#   description = "Allow SSH and HTTP"
-#   vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP and HTTPS"
+  vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     description = "SSH"
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     description = "HTTP"
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "web-sg"
-#   }
-# }
+  tags = {
+    Name = "web-sg"
+  }
+}
 
-# resource "aws_instance" "example" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   subnet_id     = aws_subnet.main.id
-#   vpc_security_group_ids = [aws_security_group.web_sg.id]
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-#   tags = {
-#     Name = "Terraform-EC2"
-#   }
-# }
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
